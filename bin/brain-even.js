0a65b00a2babf2a1cb250ecca2b0e4023cf426a8
valeriy kukushkin <valeriy9791@yandex.ru>
2023-03-10 15:21:16 +0300

    added brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const generateRandomNumber = () => Math.round(Math.random() * 100);
+
+const isEven = (number) => number % 2 === 0;
+
+const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".'; // задача игры
+
+const numberOfRounds = 3;
+
+const startGame = () => {
+  const number = generateRandomNumber();
+  const answer = isEven(number) ? 'yes' : 'no';
+  return [number, answer];
+};
+
+const getGame = () => {
+  console.log('Welkome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?  ');
+  console.log(`Hello, ${userName}!`);
+  console.log(taskGame);
+
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const [question, answer] = startGame();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== String(answer)) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+getGame();
