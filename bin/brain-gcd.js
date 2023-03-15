87d36c536d1587990b5402d0bcbbd7a41a43edbe
valeriy kukushkin <valeriy9791@yandex.ru>
2023-03-15 19:55:22 +0300

    added brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,49 @@
+#!usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const generateRandomNumber = () => { 
+    return Math.round(Math.random() * 100);
+  };
+  
+  const greatestCommonDivisor = (x, y) => {
+    while (y) {
+      let t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  };
+
+  
+  const taskGame = 'Find the greatest common divisor of given numbers.'; //задача игры
+  
+  const numberOfRounds = 3;
+  
+  const startGame = () => {
+    const number1 = generateRandomNumber();
+    const number2 = generateRandomNumber();
+    const answer = greatestCommonDivisor(number1, number2);
+    return [number1, number2, answer];
+  };
+  
+  const getGame = () => {
+    console.log('Welkome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name?  ');
+    console.log(`Hello, ${userName}!`);
+    console.log(taskGame);
+  
+    for (let i = 0; i < numberOfRounds; i += 1) {
+      const [number1, number2, answer] = startGame();
+      console.log(`Question: ${number1} ${number2}`);
+      const userAnswer = readlineSync.question('Your answer: ');
+      if (userAnswer !== String(answer)) {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+      console.log('Correct!');
+    }
+    console.log(`Congratulations, ${userName}!`);
+  };
+  getGame();
+  
