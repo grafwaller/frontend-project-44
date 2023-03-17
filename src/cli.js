8b737103e20429dfafe6b72cfc72426389385d6b
valeriy kukushkin <valeriy9791@yandex.ru>
2023-03-17 17:31:28 +0300

    update file

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
+  console.log('Welcome to the Brain Games!');
   console.log(`Hello, ${userName}!`);
 };
 
