bb1ba2de30fdea809a9b7e618b5615212d9a6d1b
valeriy kukushkin <valeriy9791@yandex.ru>
2023-03-17 17:07:06 +0300

    added file

---
+++ src/games/gsd.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+const greatestCommonDivisor = (x, y) => {
+  if (y === 0) {
+    return x;
+  }
+  return greatestCommonDivisor(y, x % y);
+};
+
+const taskGame = 'Find the greatest common divisor of given numbers.'; // задача игры
+
+const numberOfRounds = 3;
+
+const startGame = () => {
+  const number1 = generateRandomNumber();
+  const number2 = generateRandomNumber();
+  const answer = greatestCommonDivisor(number1, number2);
+  return [number1, number2, answer];
+};
+
+const getGame = () => {
+  console.log('Welkome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?  ');
+  console.log(`Hello, ${userName}!`);
+  console.log(taskGame);
+
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const [number1, number2, answer] = startGame();
+    console.log(`Question: ${number1} ${number2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== String(answer)) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default getGame;
---
+++ src/games/prime.js
@@ -0,0 +1,49 @@
+import readlineSync from 'readline-sync';
+
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".'; // задача игры
+
+const numberOfRounds = 3;
+
+const startGame = () => {
+  const number = generateRandomNumber();
+  const answer = isPrime(number) ? 'yes' : 'no';
+  return [number, answer];
+};
+
+const getGame = () => {
+  console.log('Welkome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?  ');
+  console.log(`Hello, ${userName}!`);
+  console.log(taskGame);
+
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const [question, answer] = startGame();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== String(answer)) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default getGame;
